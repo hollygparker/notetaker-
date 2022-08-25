61190e76537e5ead4e977080755be49e80e691db
Holly Parker <hollygcochran@gmail.com>
2022-08-25 14:51:01 -0500

    writing to db.json

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,66 +1,55 @@
-const app = require("express").Router();
+const router = require("express").Router();
 const fs = require("fs")
 const { v4: uuidv4 } = require('uuid');
 
 // GET route for notes
-app.get('/', (req, res) => {
+router.get('/', (req, res) => {
   // If there is no query parameter, return db file
-//   res.json("../db/db.json");
-  res.json(JSON.parse(fs.readFileSync("./db/db.json", "utf8")))
+  res.json("./db/db.json");
+//   res.json(JSON.parse(fs.readFileSync("./db/db.json", "utf8")))
 
 });
 
-// // POST request to add a note
-// app.post('/', (req, res) => {
-//   // Log that a POST request was received
-//   // console.info(`${req.method} request received to add a note`);
+// POST request to add a note
+router.post('/', (req, res) => {
+    console.log("INSIDE POST")
+  // Destructuring items in req.body
+  const { noteTitle, noteText, id } = req.body;
 
-//   // Destructuring items in req.body
-//   const { noteTitle, noteText } = req.body;
+  // If all the required properties are present
+  if (noteTitle && noteText) {
+    // Variable for the object we will save
+    const newNote = {
+      noteTitle,
+      noteText,
+      id: uuidv4()
+    };
+           
+    const stringifyNote = JSON.stringify(newNote)
+    // Obtain existing notes
+    fs.readFile('./db/db.json', 'utf8', (err, stringifyNote) => {
+      if (err) {
+        console.error("ERR in read file", err);
+      }
+      console.log("NOTES" , stringifyNote);
+        // parse string into JSON object
+        let parsedNotes = JSON.parse(stringifyNote);
+        parsedNotes.push(newNote)
+        // Write updated notes back to the file
+        fs.writeFile(
+          './db/db.json',
+          JSON.stringify(parsedNotes),
+          (err) =>
+            err
+              ? console.log(err)
+              : console.log('Successfully added note to db.json file!'))
+              res.json(parsedNotes);
+            });
+    }else{
+        res.json("err in writing note")
+    } 
+    });
 
-//   // If all the required properties are present
-//   if (noteTitle && noteText) {
-//     // Variable for the object we will save
-//     const newNote = {
-//       noteTitle,
-//       noteText,
-//       id: uuidv4(),
-//     };
 
-//     // id: uuidv4(),
-//     // Obtain existing notes
-//     fs.readFile('./db/db.json', 'utf8', (err, data) => {
-//       if (err) {
-//         console.error(err);
-//       } else {
-//         // Convert string into JSON object
-//         let parsedNotes = JSON.parse(data);
+    module.exports = router
 
-//         // Add a new note
-//         parsedNotes.push(newNote);
-
-//         // Write updated notes back to the file
-//         fs.writeFile(
-//           './db/db.json',
-//           JSON.stringify(parsedNotes),
-//           (writeErr) =>
-//             writeErr
-//               ? console.error(writeErr)
-//               : console.info('Successfully updated notes!')
-//         );
-//       }
-//       res.JSON(parsedNotes);
-//     });
-
-//     const response = {
-//       status: 'success',
-//       body: newNote,
-//     };
-
-//     console.log(response);
-//     res.status(201).json(response);
-//   } else {
-//     res.status(500).json('Error in posting note');
-//   }
-// });
-module.exports = app
