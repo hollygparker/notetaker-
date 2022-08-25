3e54d23fa0ccc311639c091ea86ebe540a6b60b3
Holly Parker <hollygcochran@gmail.com>
2022-08-24 22:26:48 -0500

    adding html routes to server

--- public/server.js
+++ public/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 const noteData = require('../db/db.json');
-
+const uuid = require('../helpers/uuid');
 const app = express();
 const PORT = 3001;
 
@@ -18,8 +18,8 @@ app.get('*', (req, res) =>
 )
 
 // GET route to get all of the notes or all the terms sorted
-app.get('/api/notes/', (req, res) => {
-  // console.log(req.params, 'first');
+app.get('/api/notes', (req, res) => {
+  console.log(res, 'first');
 
   // Check and see if there is a query parameter at all
   const hasQuery = Object.keys(req.query).length > 0;
@@ -37,6 +37,67 @@ app.get('/api/notes/', (req, res) => {
   // If there is no query parameter, return db file
   return res.json(noteData);
 });
+
+// POST request to add a review
+app.post('/api/notes', (req, res) => {
+  // Log that a POST request was received
+  console.info(`${req.method} request received to add a note`);
+
+  // Destructuring assignment for the items in req.body
+  const { noteTitle, noteText } = req.body;
+
+  // If all the required properties are present
+  if (noteTitle && noteText) {
+    // Variable for the object we will save
+    const newNote = {
+      noteTitle,
+      noteText,
+      review_id: uuid(),
+    };
+
+    // Obtain existing reviews
+    fs.readFile('../db/db.json', 'utf8', (err, data) => {
+      if (err) {
+        console.error(err);
+      } else {
+        // Convert string into JSON object
+        const parsedNotes = JSON.parse(data);
+
+        // Add a new review
+        parsedNotes.push(newNote);
+
+        // Write updated notes back to the file
+        fs.writeFile(
+          '../db/db.json',
+          JSON.stringify(parsedNotes, null, 4),
+          (writeErr) =>
+            writeErr
+              ? console.error(writeErr)
+              : console.info('Successfully updated notes!')
+        );
+      }
+    });
+
+    const response = {
+      status: 'success',
+      body: newNote,
+    };
+
+    console.log(response);
+    res.status(201).json(response);
+  } else {
+    res.status(500).json('Error in posting note');
+  }
+});
+
+// * // POST request for reviews
+// app.post('/api/notes', (req, res) => {
+//   // Inform the client that their POST request was received
+//   res.json(`${req.method} request received to add a note`);
+
+//   // Log our request to the terminal
+//   console.info(`${req.method} request received to add a note`);
+// });
 
 // app.get('notes.html', (req, res) =>
 //   res.sendFile(path.join(__dirname, 'notes.html'))
